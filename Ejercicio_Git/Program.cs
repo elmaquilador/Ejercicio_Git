4e7a63207d909a385eabd7d56cc3dfa292f2cccc
El Maquilador <elmaquilador@outlook.com>
2025-08-28 17:50:32 -0600

    chore: restaurar proyecto al estado anterior 5fad53e8
    modified:   Ejercicio_Git/Program.cs

--- Ejercicio_Git/Program.cs
+++ Ejercicio_Git/Program.cs
@@ -10,47 +10,7 @@ namespace Ejercicio_Git
     {
         static void Main(string[] args)
         {
-            double resultado, num1 = 10, num2 = 20;
 
-            Console.WriteLine("1. Suma");
-            Console.WriteLine("2. Resta");
-            Console.WriteLine("3. Multiplicación");
-
-            Console.Write("Elige una opción: ");
-            int opcion = Convert.ToInt32(Console.ReadLine());
-
-            switch (opcion)
-            {
-                case 1:
-                    resultado = Suma(num1, num2);
-                    Console.WriteLine($"Resultado de la suma: {resultado}");
-                    break;
-                case 2:
-                    resultado = Resta(num1, num2);
-                    Console.WriteLine($"Resultado de la resta: {resultado}");
-                    break;
-                case 3:
-                    resultado = Multiplicacion(num1, num2);
-                    Console.WriteLine($"Resultado de la multiplicación: {resultado}");
-                    break;
-                default:
-                    Console.WriteLine("Opción no válida.");
-                    break;
-            }
-        }
-
-        static double Suma (double num1, double num2)
-        {
-            double resultado = num1 + num2;
-
-            return resultado;
-        }
-        
-        static double Resta (double num1, double num2)
-        {
-            double resultado = num1 - num2;
-
-            return resultado;
         }
     }
 }
